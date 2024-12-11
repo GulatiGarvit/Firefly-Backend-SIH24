429bca9813ac6a1ac1668b65b5f2d6e18adbd4f8
Garvit Gulati <ggulati_be22@thapar.edu>
2024-12-11 14:40:07 +0530

    minor fix

--- services/fire_station.js
+++ services/fire_station.js
@@ -2,13 +2,13 @@ const { sequelize, FireStation } = require("../models");
 const NotificationService = require("./notification");
 
 const createAlertForIncident = async (incident) => {
-	// Get the incident's building id
-	const buildingId = incident.buildingId;
+    // Get the incident's building id
+    const buildingId = incident.buildingId;
 
-	// Run Geometry query to find nearest fire station to the building (comparing their latlng)
-	// Using SQL's Geometry("Point") functions
-	const nearestFireStation = await sequelize.query(
-		`
+    // Run Geometry query to find nearest fire station to the building (comparing their latlng)
+    // Using SQL's Geometry("Point") functions
+    const nearestFireStation = await sequelize.query(
+        `
         SELECT * 
         FROM fire_stations 
         ORDER BY ST_Distance(
@@ -17,30 +17,30 @@ const createAlertForIncident = async (incident) => {
         ) 
         LIMIT 1
         `,
-		{
-			replacements: { buildingId }, // Pass the buildingId safely into the query
-			type: sequelize.QueryTypes.SELECT, // Specify query type for SELECT
-			mapToModel: true, // Map the result to the FireStation model
-			model: FireStation,
-		}
-	);
+        {
+            replacements: { buildingId }, // Pass the buildingId safely into the query
+            type: sequelize.QueryTypes.SELECT, // Specify query type for SELECT
+            mapToModel: true, // Map the result to the FireStation model
+            model: FireStation,
+        }
+    );
 
-	// TODO: Formulate the message
-	const data = {
-		gotNewIncident: true,
-	};
+    // TODO: Formulate the message
+    const data = {
+        gotNewIncident: "true",
+    };
 
-	// Notify the fire station's
-	// Nearest firestion to be added to incident
-	incident.fireStationId = nearestFireStation[0].id;
-	console.log(incident.fireStationId);
-	await incident.save();
-	await NotificationService.sendNotification(
-		nearestFireStation[0].fcmToken,
-		data
-	);
+    // Notify the fire station's
+    // Nearest firestion to be added to incident
+    incident.fireStationId = nearestFireStation[0].id;
+    console.log(incident.fireStationId);
+    await incident.save();
+    await NotificationService.sendNotification(
+        nearestFireStation[0].fcmToken,
+        data
+    );
 };
 
 module.exports = {
-	createAlertForIncident,
+    createAlertForIncident,
 };
