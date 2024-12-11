a5c0eb1ad0cbd9e270c52ca91238aa8ca45a007b
Garvit Gulati <ggulati_be22@thapar.edu>
2024-12-11 13:53:56 +0530

    add incident id to notif

--- services/user.js
+++ services/user.js
@@ -2,27 +2,28 @@ const { User } = require("../models");
 const NotificationService = require("./notification");
 
 const createAlertForIncident = async (incident) => {
-	// Find building for the incident (only need city)
-	const building = await incident.getBuilding({ include: ["city"] });
-	const city = building.city;
+    // Find building for the incident (only need city)
+    const building = await incident.getBuilding({ include: ["city"] });
+    const city = building.city;
 
-	// Find all users in the city
-	const users = await User.findAll({
-		where: { cityId: city.id },
-		attributes: ["fcmToken"],
-	});
+    // Find all users in the city
+    const users = await User.findAll({
+        where: { cityId: city.id },
+        attributes: ["fcmToken"],
+    });
 
-	// TODO: Formulate the message
-	const data = {
-		gotNewIncident: true,
-	};
+    // TODO: Formulate the message
+    const data = {
+        gotNewIncident: true,
+        incidentId: incident.id,
+    };
 
-	// For each user in users send notif
-	for (let user of users) {
-		await NotificationService.sendNotification(user.fcmToken, data);
-	}
+    // For each user in users send notif
+    for (let user of users) {
+        await NotificationService.sendNotification(user.fcmToken, data);
+    }
 };
 
 module.exports = {
-	createAlertForIncident,
+    createAlertForIncident,
 };
